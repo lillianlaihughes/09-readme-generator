e7809568d32dd1a95ed58132fe2159dc538ba75b
lillianlaihughes <lillianlaihughes@gmail.com>
2023-03-26 16:32:48 -0400

    upated js and md files

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const generateMarkdown = require('./utils/generateMarkdown')
 const questions = [
     {
         type: 'input',
-        name: 'name',
-        message: 'What is your name?',
+        name: 'project-title',
+        message: 'Enter your Project Title.',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a short description of your Project.',
     },
     {
         type: 'input',
@@ -17,13 +22,8 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'name',
-        message: 'What is your name?',
-    },
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?',
+        name: 'github',
+        message: 'What is your Github username?',
     },
     {
         type: 'input',
@@ -40,11 +40,18 @@ const questions = [
 // TODO: Create a function to write README file
 // ask DMG about fs.writeFile versus this
 function writeToFile(filename, data) {
-
+    return fs.writeFileSync(path.join(process.cwd(), filename), data); 
+    // cwd = current working directory 
+    // REVIEW writeFileSync doesn't require a callback param like writeFile does
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.createPromptModule(questions).then((inquirerResponses) => {
+        console.log('Generating README file...')
+        writeToFile('README.md', generateMarkdown({...inquirerResponses})); // REVIEW ... rest/spread
+        })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,73 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `[License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`; // add url and insert ${} for license name
+  }
+  return '';
+}
+
+// [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `\n* [License](#license)\n`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
+    This project is licensed under the ${license} license.`;
+   }
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+
+* [Usage](#usage)
+${renderLicenseLink(data.license)}
+
+* [Contributing](#contributing)
+
+* [Tests] (#tests)
+
+* [Questions](#questions)
+
+## Installation
+To install necessary dependencies, run the following command:
+```
+${data.installion}
+```
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+To run tests, run the following command:
+```
+${data.test}
+```
+
+## Questions
+If you have any questions about this repo, open an issue or send a message to ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
 `;
 }
 
